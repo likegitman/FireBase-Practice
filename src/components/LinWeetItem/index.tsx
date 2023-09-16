d7977a48a23856ab4da7ea2591a16573a0aa303d
unknown <momlin069@gmail.com>
2023-09-16 23:59:22 +0900

    :sparkles: Add delete weet

--- src/components/LinWeetItem/index.tsx
+++ src/components/LinWeetItem/index.tsx
@@ -1,3 +1,5 @@
+import { deleteDoc, doc } from 'firebase/firestore'
+import { dbService } from '../../firebase'
 import { GetWeetsTypes } from '../../types/GetWeetsTypes'
 import * as S from './style'
 
@@ -8,13 +10,19 @@ const LinWeetItem = ({
   weet: GetWeetsTypes
   isMine: boolean
 }) => {
+  const onDelete = async () => {
+    const ok = window.confirm('Are you sure you watn to delete thsi nweet?')
+    if (ok) {
+      await deleteDoc(doc(dbService, `linweets/${weet.id}`))
+    }
+  }
   return (
     <S.LinWeetItemBox>
       <h4>{weet.text}</h4>
       {isMine && (
         <>
           <button>Edit Weet</button>
-          <button>Delete Weet</button>
+          <button onClick={onDelete}>Delete Weet</button>
         </>
       )}
     </S.LinWeetItemBox>
